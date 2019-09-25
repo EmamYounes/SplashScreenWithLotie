246f747603684e503bda53fadf44e1bd053083f7
Emam <emamAhmed36@gmail.com>
2019-09-25 16:13:11 +0200

    add error popup

--- app/src/main/java/com/example/splashscreenwithlotie/MainActivity.java
+++ app/src/main/java/com/example/splashscreenwithlotie/MainActivity.java
@@ -9,24 +9,24 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.airbnb.lottie.LottieAnimationView;
-
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button successBtn;
+    Button errorBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.bt_success);
+        successBtn = findViewById(R.id.bt_success);
+        errorBtn = findViewById(R.id.error);
         final Dialog settingsDialog = new Dialog(MainActivity.this);
         settingsDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        settingsDialog.setContentView(getLayoutInflater().inflate(R.layout.image_layout
-                , null));
-        button.setOnClickListener(new View.OnClickListener() {
+        successBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                settingsDialog.setContentView(getLayoutInflater().inflate(R.layout.success_image_layout
+                        , null));
                 settingsDialog.show();
                 new Handler().postDelayed(new Runnable() {
                     @Override
@@ -34,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
                         settingsDialog.dismiss();
                     }
                 }, 4500);
+            }
+        });
+
+        errorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                settingsDialog.setContentView(getLayoutInflater().inflate(R.layout.error_image_layout
+                        , null));
+                settingsDialog.show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        settingsDialog.dismiss();
+                    }
+                }, 2800);
+
             }
         });
     }
